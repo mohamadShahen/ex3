a902b2ca6b4f3582b2d5b8361c4ae7d9d76ad51d
mohamadShahen <118079548+mohamadShahen@users.noreply.github.com>
2022-12-25 13:43:44 +0200

    after basic tests success

--- HealthPoints.cpp
+++ HealthPoints.cpp
@@ -1,35 +1,172 @@
-
 #include "HealthPoints.h"
 
-HealthPoints& HealthPoints::operator=(const int value) {
+HealthPoints::HealthPoints(int maxHP)
+{
+    if (maxHP <= 0){
+        throw InvalidArgument();
+    }
+    else{
+        m_maxHP = maxHP;
+        m_currentHP = m_maxHP;
+    }
+}
+
+HealthPoints& HealthPoints::operator=(const int value){
+    m_maxHP = 100;
     if(value <= m_maxHP && value >= 0){
         m_currentHP = value;
+    }
+    else if(value < 0){
+        m_currentHP = 0;
+    }
+    else if(value > m_maxHP){
+        m_currentHP = m_maxHP;
     }
 
     return *this;
 }
 
-bool HealthPoints::operator==(HealthPoints& hp) const {
+bool operator==(const int value,const HealthPoints& hp)
+{
+    return (value == hp.m_currentHP);
+}
+
+bool HealthPoints::operator==(const int hp) const
+{
+    return (m_currentHP == hp);
+}
+
+bool HealthPoints::operator==(const HealthPoints& hp) const
+{
     return (hp.m_currentHP == m_currentHP);
 }
 
-bool HealthPoints::operator>=(HealthPoints& hp) const {
+bool operator!=(const int value,const HealthPoints& hp)
+{
+    return(hp.m_currentHP != value);
+}
+
+bool HealthPoints::operator!=(const int hp) const
+{
+    return (hp != m_currentHP);
+}
+
+bool HealthPoints::operator!=(const HealthPoints& hp) const
+{
+    return (hp.m_currentHP != m_currentHP);
+}
+
+bool HealthPoints::operator>=(const HealthPoints& hp) const
+{
     return (m_currentHP >= hp.m_currentHP);
 }
 
-bool HealthPoints::operator>(HealthPoints& hp) const {
+bool HealthPoints::operator>=(const int value) const
+{
+    return (m_currentHP >= value);
+}
+
+bool operator>=(const int value,const HealthPoints& hp)
+{
+    return (value <= hp.m_currentHP);
+}
+
+bool HealthPoints::operator>(const HealthPoints& hp) const
+{
     return (m_currentHP > hp.m_currentHP);
 }
 
-bool HealthPoints::operator<(HealthPoints& hp) const {
+bool HealthPoints::operator>(const int value) const
+{
+    return (m_currentHP > value);
+}
+
+bool operator>(const int value,const HealthPoints& hp){
+    return (value > hp.m_currentHP);
+}
+
+bool HealthPoints::operator<(const HealthPoints& hp) const
+{
     return (m_currentHP < hp.m_currentHP);
 }
 
-bool HealthPoints::operator<=(HealthPoints& hp) const {
+bool HealthPoints::operator<(const int value) const
+{
+    return (m_currentHP < value);
+}
+
+bool operator<(const int value, const HealthPoints& hp)
+{
+    return (value < hp.m_currentHP);
+}
+
+bool HealthPoints::operator<=(const HealthPoints& hp) const
+{
     return (m_currentHP <= hp.m_currentHP);
 }
 
-std::ostream& operator<<(std::ostream& os, const HealthPoints& hp) {
-    os << hp.m_currentHP << "(" << hp.m_maxHP << ")";
+bool HealthPoints::operator<=(const int value) const
+{
+    return (m_currentHP <= value);
+}
+
+bool operator<=(const int value,const HealthPoints& hp)
+{
+    return (value <= hp.m_currentHP);
+}
+
+std::ostream& operator<<(std::ostream& os, const HealthPoints& hp)
+{
+    os << hp.m_currentHP << '(' << hp.m_maxHP << ')';
     return os;
+}
+
+HealthPoints& HealthPoints::operator+=(const int& value)
+{
+    m_currentHP += value;
+    if (m_currentHP > m_maxHP){
+        m_currentHP = m_maxHP;
+        return *this;
+    }
+    return *this;
+}
+
+HealthPoints& HealthPoints::operator-=(const int& value)
+{
+    m_currentHP -= value;
+    if (m_currentHP < 0){
+        m_currentHP = 0;
+        return *this;
+    }
+    return *this;
+}
+
+
+HealthPoints& HealthPoints::operator+(const int& value)
+{
+    m_currentHP += value;
+    if (m_currentHP > m_maxHP){
+        m_currentHP = m_maxHP;
+    }
+    return *this;
+}
+
+int operator+(const int& value, HealthPoints& health)
+{
+    int temp = value + health.m_currentHP;
+    return temp;
+}
+
+HealthPoints& HealthPoints::operator-(const int& value)
+{
+    m_currentHP -= value;
+    if (m_currentHP < 0){
+        m_currentHP = 0;
+    }
+    return *this;
+}
+
+int operator-(const int& value, HealthPoints& health) {
+    int temp = value - health.m_currentHP;
+    return temp;
 }
--- HealthPoints.h
+++ HealthPoints.h
@@ -8,18 +8,39 @@ class HealthPoints{
     int m_currentHP;
     int m_maxHP;
     friend std::ostream& operator<<(std::ostream&, const HealthPoints&);
-public:
-    HealthPoints(int maxHP = 100);
-    HealthPoints(HealthPoints&) = default;
-    ~HealthPoints() = default;
+    friend int operator+(const int&, HealthPoints&);
+    friend int operator-(const int&, HealthPoints&);
+    friend bool operator==(const int ,const HealthPoints&);
+    friend bool operator!=(const int ,const HealthPoints&);
+    friend bool operator>=(const int ,const HealthPoints&);
+    friend bool operator>(const int ,const HealthPoints&);
+    friend bool operator<=(const int ,const HealthPoints&);
+    friend bool operator<(const int, const HealthPoints&);
 
+public:
+    class InvalidArgument{};
+    HealthPoints(int maxHP = 100);
+    HealthPoints(const HealthPoints&) = default;
+    ~HealthPoints() = default;
+    HealthPoints& operator+=(const int&);
+    HealthPoints& operator+=(const HealthPoints&) = delete;
+    HealthPoints& operator-=(const int&);
+    HealthPoints& operator-(const int&);
+    HealthPoints& operator+(const int&);
     HealthPoints& operator=(int);
     HealthPoints& operator=(const HealthPoints&) = default;
-    bool operator==(HealthPoints&) const;
-    bool operator>(HealthPoints&) const;
-    bool operator<(HealthPoints&) const;
-    bool operator>=(HealthPoints&) const;
-    bool operator<=(HealthPoints&) const;
+    bool operator==(const int) const;
+    bool operator==(const HealthPoints&) const;
+    bool operator!=(const int) const;
+    bool operator!=(const HealthPoints&) const;
+    bool operator>(const HealthPoints&) const;
+    bool operator>(const int) const;
+    bool operator<(const HealthPoints&) const;
+    bool operator<(const int) const;
+    bool operator>=(const HealthPoints&) const;
+    bool operator>=(const int) const;
+    bool operator<=(const HealthPoints&) const;
+    bool operator<=(const int) const;
 };
 
 #endif //EX3_HEALTHPOINTS_H
